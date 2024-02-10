5d1c23fdadfa365e6b551101ae3dd61b6ac448e4
madirusso <madisonrusso2023@u.northwestern.edu>
2024-02-09 19:36:21 -0600

    deploy new webpage

---
+++ assets/js/grain.js
@@ -0,0 +1,12 @@
+var options = {
+    animate: true,
+    patternWidth: 100,
+    patternHeight: 100,
+    grainOpacity: 0.05,
+    grainDensity: 1,
+    grainWidth: 1,
+    grainHeight: 1
+};
+
+grained('#wrapper', options);
+
